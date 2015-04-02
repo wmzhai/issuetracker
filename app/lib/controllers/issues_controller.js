c9a6ce198306247aea87a937ed43efff66d7776a
Weiming Zhai <wmzhai@gmail.com>
2015-04-02 13:53:22 +0800

    implementing issues controller

--- app/lib/controllers/issues_controller.js
+++ app/lib/controllers/issues_controller.js
@@ -9,12 +9,13 @@ IssuesController = RouteController.extend({
     // the subscription handle is added to a reactive list
     // and when all items in this list are ready, this.ready()
     // returns true in any of your route functions.
-
+    this.subscribe('issues', Meteor.userId());
   },
 
   data: function () {
     // return a global data context like this:
     // Items.findOne({_id: this.params._id});
+    return Issues.findOne({_id: this.params._id});
   },
 
   insert: function () {
